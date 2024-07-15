37e8b155dafd0b9eb9e6995703a0ead814da30a6
Supun Nethsara <supunnethsara410@gmail.com>
2024-07-15 00:24:57 -0700

    Update koombiyoApi.php

--- app/Services/koombiyoApi.php
+++ app/Services/koombiyoApi.php
@@ -6,23 +6,33 @@ use App\Models\User;
 use Illuminate\Support\Facades\Http;
 use Filament\Notifications\Notification;
 use Filament\Notifications\Actions\Action;
+use GuzzleHttp\Client;
 
 class koombiyoApi
 {
     //get barcodes
     public function getAllAllocatedBarcodes($apikey)
     {
-        $response = Http::asForm()->post('https://application.koombiyodelivery.lk/api/Waybils/users', [
-            'apikey' => $apikey,
-            'limit' => 1,
-        ]);
+        $client = new Client();
+        $url = 'https://application.koombiyodelivery.lk/api/Waybils/users';
 
-        if ($response->successful()) {
-            $barcode = $response->json();
-            return collect($barcode);
+        try {
+            $response = $client->post($url, [
+                'form_params' => [
+                    'apikey' => $apikey,
+                    'limit' => 1,
+                ],
+            ]);
+
+            if ($response->getStatusCode() == 200) {
+                $barcode = json_decode($response->getBody()->getContents(), true);
+                return collect($barcode);
+            } else {
+                return response()->json(['error' => 'Unable to fetch data'], $response->getStatusCode());
+            }
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An error occurred while fetching data'], 500);
         }
-
-        return response()->json(['error' => 'Unable to fetch data'], $response->status());
     }
 
     //get all district
